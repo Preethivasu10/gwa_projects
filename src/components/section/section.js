8c3fb96672d846bb4de6f80eb1ed9f6a22fe23f2
Loga priya Eswaran <logapriyaeswaran@Logas-MacBook-Air.local>
2024-06-20 13:55:37 +0530

    insight page is added

--- src/components/section/section.js
+++ src/components/section/section.js
@@ -6,14 +6,21 @@ import TestimonialSlider from '../section5/TestimonialSlider';
 import Footerr from '../footer/footer';
 import Footer from '../footer/footerrs';
 import WhyUs from '../section.6/Whyus';
+import Insights from '../Insight/Insights';
+
+
 // import Insights from '../section.6/Appp';
 
 function Section() {
   return ( 
     <div className="Container">
       <div>
-      <WhyUs/></div>
+      <WhyUs/>
+
+      </div>
+<Insights/>
       <TestimonialSlider/>
+   
       <CenteredContent/>
       <Footerr/>
     <Footer/>

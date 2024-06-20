6a9ac91e3694984e40fa576ebda7021797237560
Preethivasu10 <preethivasu2020@gmail.com>
2024-06-20 12:41:22 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+
+import './App.css';
+import Section from './components/section/section';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <Section/>
+  
+  );
+}
+
+export default App;
